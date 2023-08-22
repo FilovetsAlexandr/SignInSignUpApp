10a13f528efd213686cc31275f7b6539a402e86b
Acidsasha <sohan.boxan@gmail.com>
2023-08-23 01:27:35 +0300

    Project created

---
+++ MySignInSignUpApp/MySignInSignUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySignInSignUpApp
+//
+//  Created by MacBookPro15 on 23.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
